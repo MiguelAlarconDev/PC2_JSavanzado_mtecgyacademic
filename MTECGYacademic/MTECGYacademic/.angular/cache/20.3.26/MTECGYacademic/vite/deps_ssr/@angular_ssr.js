66131c383b4cf401559cedf85d06beec2847bfc9
Miguel Alarcon <miguel.alarconescriba@gmail.com>
2026-05-28 23:01:54 -0500

    Ultimo cambio Miguel 28/05

---
+++ MTECGYacademic/MTECGYacademic/.angular/cache/20.3.26/MTECGYacademic/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,41 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-2K47DEWM.js";
+import "./chunk-KOT2BH4L.js";
+import "./chunk-DKAITSPT.js";
+import "./chunk-H77WMAPP.js";
+import "./chunk-HKDETGSG.js";
+import "./chunk-UO4TC4JF.js";
+import "./chunk-23BBIQPH.js";
+import "./chunk-NBYIBYKZ.js";
+import "./chunk-JJCEQHS6.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
---
+++ MTECGYacademic/MTECGYacademic/.angular/cache/20.3.26/MTECGYacademic/vite/deps_ssr/rxjs.js
@@ -0,0 +1,6 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  require_cjs
+} from "./chunk-NBYIBYKZ.js";
+import "./chunk-JJCEQHS6.js";
+export default require_cjs();
